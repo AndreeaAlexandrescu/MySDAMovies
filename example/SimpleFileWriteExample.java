ed97cf24136b28233ff6e27a4fabc1bd319997b7
Andreea <rk_deea88@yahoo.com>
2019-05-11 23:07:12 +0300

    first commit

---
+++ example/SimpleFileWriteExample.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class SimpleFileWriteExample {
+    public static void main(String[] args) {
+        String relativePath = "out.txt";
+        File file = new File(relativePath);
+
+        try {
+            String message1 = "message 1";
+            String message2 = "message 2";
+            String message3 = "message 3";
+
+
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(message1);
+            bufferedWriter.newLine();
+            bufferedWriter.write(message2);
+            bufferedWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+}
